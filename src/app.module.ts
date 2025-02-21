f3f7bfd79932b07a0a2be259136307da1d30349f
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-21 12:03:24 -0300

    feat: added mongodb to project
    - updated env files
    - created docker compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URL'),
+        dbName: configService.get('MONGODB_NAME'),
+        user: configService.get('MONGODB_USER'),
+        pass: configService.get('MONGODB_PASSWORD'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
